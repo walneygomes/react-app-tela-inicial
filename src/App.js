9bc964b1dc2e672f6e6227e45bc0db242fca0206
walneygomes <walneycg@gmail.com>
2021-11-02 23:29:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -13,100 +13,58 @@ function App() {
      
      
       <p></p>
-      <div className="video"> 
-      
-      <p>      VIDEO 01 - CRIAÇÃO DO MAVEN PROJECT
-</p>
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/D0EfZeCDDoA"  allowfullscreen></iframe>
-
-      
-      </div>
+     
 
 
       <p></p>
       <div className="video"> 
         
     
-        <p>    VIDEO 02 - CRIAÇÃO DO MAIN APPLICATION</p>
+        <p>    VIDEO 01 - CRIAÇÃO DO MAVEN PROJECT E  MAIN APPLICATION</p>
 
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/3XQyCPtYhLg"  allowfullscreen></iframe>
+        <iframe width="853" height="480" src="https://www.youtube.com/embed/oAQZLOqDwQU"  allowfullscreen></iframe>
 
         </div>
         <p></p>
 
 
       <div className="video"> 
-        <p>VIDEO 03 - CRIAÇÃO DO REPOSITORIO</p>
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/iP2wvO4jCvs"  allowfullscreen></iframe>
+        <p>VIDEO 02 - CRIAÇÃO DA ENTIDADE E REPOSITORIO</p>
+        <iframe width="853" height="480" src="https://www.youtube.com/embed/yarzQZ1DkiA"  allowfullscreen></iframe>
 
         </div>
         <p></p>
 
       <div className="video"> 
-      <p>VIDEO 04 - CRIAÇÃO DA ENTIDADE</p>
+      <p>VIDEO 03 - CRIAÇÃO DO SERVICE</p>
 
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/bcz8j7O_gDU"  allowfullscreen></iframe>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/_EHqvwtkXcs"  allowfullscreen></iframe>
 
       </div>
 
       <p></p>
 
       <div className="video">
-      <p> VIDEO 05 - CRIAÇÃO DO SERVICE PARTE 01</p>
+      <p> VIDEO 04 - CRIAÇÃO DO CONTROLLER</p>
 
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/Q4XVr952Y44"  allowfullscreen></iframe>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/1SuSPuxocpA"  allowfullscreen></iframe>
 
       </div>
-      <p></p>
-
-      <div className="video"> 
-      <p>VIDEO 06 - CRIAÇÃO DO SERVICE PARTE 02</p>
-
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/5K65JOs9uMw"  allowfullscreen></iframe>
-
       
-      </div>
       <p></p>
+      <div className="video">
+      <p> VIDEO 04 - CRIAÇÃO DO application.properties</p>
 
-      <div className="video"> 
-      <p>VIDEO 07 - CRIAÇÃO DO SERVICE PARTE 03</p>
-
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/twq2g2Gz6yg"  allowfullscreen></iframe>
-
-      
-      </div>
-      <p></p>
-
-      <div className="video">      <p> VIDEO 08 - CRIAÇÃO DO CONTROLLER PARTE 01</p>
-
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/Cq8nX-d_w7M"  allowfullscreen></iframe>
-
-      
-      </div>
-      <p></p>
-
-      <div className="video">      <p> VIDEO 09 - CRIAÇÃO DO CONTROLLER PARTE 02</p>
-
-      
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/eAkPabvBAwg"  allowfullscreen></iframe>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/6C_ceoc0FvE"  allowfullscreen></iframe>
 
       </div>
-      <p></p>
+      <div className="video">
+      <p> VIDEO 04 - TESTES COM POSTMAN</p>
 
-      <div className="video"> 
-      <p>VIDEO 10 - CRIANDO O FILE PROPERTIES</p>
-
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/eWRAgAouFMs"  allowfullscreen></iframe>
-</div>
-      <p></p>
-
-      <div className="video"> 
-      <p>VIDEO 11 - TESTANDO O WEBSERVICE COM POSTMAN</p>
-
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/wjkVSCUuNDA"  allowfullscreen></iframe>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/l1EHNmGWBcA"  allowfullscreen></iframe>
 
       </div>
-      <p></p>
+
       <p></p>
 
       <div className="Rodape">   Criado por: Walney de Negreiros Gomes</div>
